1091003f585416fb56481855ded05b097977a7a2
briankipchirchir <kbrianngetich@gmail.com>
2023-03-15 00:09:38 +0300

    project

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 =1
+let num2 =62
+let multiply = num1*num2
+return multiply
+
+let random =Math.floor(math.random()*100)+1
+return random
+
+let num3 =25
+let num4 =7
+let mod =num3%num4
+return mod
+
+let max =Math.max(5,6,9,14,20)
+return max
